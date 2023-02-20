2d932d924ee20654863dfa63312ebdf004efca9f
JohnnyLoeve <120627383+JohnnyLoeve@users.noreply.github.com>
2023-02-20 14:50:34 +0100

    Did a bit of Security

--- src/main/java/com/example/SharingIsCaring/MainController.java
+++ src/main/java/com/example/SharingIsCaring/MainController.java
@@ -1,22 +1,31 @@
 package com.example.SharingIsCaring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
 
     //GetMapping for HomePage
-
-
+    @GetMapping("/")
+    public String mainPage(){
+        return"MainPage";
+    }
     //GetMapping for CreateUser
 
     //PostMapping for CreateUser
 
 
     //GetMapping for Login
-
+    @GetMapping("/LoginPage")
+    public String loginPage(){
+        return "LoginPage";
+    }
     //PostMapping for Login
-
+//    @PostMapping("/LoginPage")
+//    public String Login(){
+//    }
     //GetMapping for Logout
 
 
--- src/main/java/com/example/SharingIsCaring/SecurityConfig.java
+++ src/main/java/com/example/SharingIsCaring/SecurityConfig.java
@@ -1,4 +1,48 @@
 package com.example.SharingIsCaring;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-public class SecurityConfig extends {
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/Account", "/", "/CreateAMeal", "/CreateUser", "/ContactForm", "/LoginPage", "/MealOptions").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/LoginPage")
+                        .permitAll()
+                )
+                .logout((logout) -> logout.permitAll());
+
+                //.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withDefaultPasswordEncoder().username("Lisa").password("123").roles("ADMIN").build());
+        manager.createUser(User.withDefaultPasswordEncoder().username("Sion").password("123").roles("ADMIN").build());
+        return manager;
+    }
 }
