9036ff808497831649b411d5145e0a09a64df58f
JohnnyLoeve <120627383+JohnnyLoeve@users.noreply.github.com>
2023-02-20 13:05:40 +0100

    Scaffold set

---
+++ src/main/java/com/example/SharingIsCaring/MainController.java
@@ -0,0 +1,45 @@
+package com.example.SharingIsCaring;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+    //GetMapping for HomePage
+
+
+    //GetMapping for CreateUser
+
+    //PostMapping for CreateUser
+
+
+    //GetMapping for Login
+
+    //PostMapping for Login
+
+    //GetMapping for Logout
+
+
+    //GetMapping for Account
+    //(Use an "id" to get the propper user, and/or meal)
+
+
+    //GetMapping for MealOptions
+
+
+    //GetMapping for ContactForm
+    //PostMapping for ContactForm
+
+
+    //GetMapping for CreateAMeal
+
+    //PostMapping for CreateAMeal (for at gemme måltidet)
+
+    //GetMapping for EditMeal
+
+    //PostMapping for EditMeal (Eller bare gøre brug af postmapping fra create a meal)
+
+    //GetMapping for DeleteAMeal
+
+
+}
---
+++ src/main/java/com/example/SharingIsCaring/SecurityConfig.java
@@ -0,0 +1,4 @@
+package com.example.SharingIsCaring;
+
+public class SecurityConfig extends {
+}
