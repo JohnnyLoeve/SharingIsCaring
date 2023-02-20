33730aa877d74c75920257b87550f1ffa5b73037
JohnnyLoeve <120627383+JohnnyLoeve@users.noreply.github.com>
2023-02-20 12:13:31 +0100

    Initial commit

---
+++ src/test/java/com/example/SharingIsCaring/SharingIsCaringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SharingIsCaring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharingIsCaringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
